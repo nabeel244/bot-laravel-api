15fccd51807dd8380e20d1cb8309803afee22cc6
Full Stack Laravel | React | Node | Nest | Next | Wordpress Developer <72126136+nabeel244@users.noreply.github.com>
2024-12-30 12:44:44 +0500

    add test users

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -8,6 +8,11 @@ use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
+    public function getAllUsers()
+    {
+        $users = TelegramUser::all(); // Fetch all users
+        return response()->json(['status' => 'ok', 'users' => $users]);
+    }
     public function telegramUser(Request $request)
     {
         $validated = $request->validate([
